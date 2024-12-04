1df6e336d3b7989a694937bdcc00f4c7833be8b7
Kirill Akhmetov <kirillahmetov935@gmail.com>
2024-12-04 17:39:31 +0300

    change impl Filter to HttpFilter

--- src/main/java/ru/kpfu/itis/kirillakhmetov/filter/CharsetFilter.java
+++ src/main/java/ru/kpfu/itis/kirillakhmetov/filter/CharsetFilter.java
@@ -1,17 +1,21 @@
 package ru.kpfu.itis.kirillakhmetov.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @WebFilter("/*")
-public class CharsetFilter implements Filter {
+public class CharsetFilter extends HttpFilter {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        servletResponse.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        filterChain.doFilter(servletRequest, servletResponse);
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        res.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        chain.doFilter(req, res);
     }
 }
