8a13c3edee10f70804d04e07dd2275b356e64f7d
Kirill Akhmetov <kirillahmetov935@gmail.com>
2024-12-04 17:39:06 +0300

    added security filter

---
+++ src/main/java/ru/kpfu/itis/kirillakhmetov/filter/SecurityFilter.java
@@ -0,0 +1,43 @@
+package ru.kpfu.itis.kirillakhmetov.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.kpfu.itis.kirillakhmetov.service.SecurityService;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class SecurityFilter extends HttpFilter {
+    private static final String[] SECURITY_PATHS = {"/login", "/registration",
+            "/style/login.css", "/style/registration.css"};
+    private SecurityService securityService;
+
+    @Override
+    public void init() throws ServletException {
+        securityService = (SecurityService) getServletContext().getAttribute("securityService");
+    }
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        boolean matchPaths = false;
+        for (String path : SECURITY_PATHS) {
+            if (path.equals(req.getRequestURI().substring(req.getContextPath().length()))) {
+                matchPaths = true;
+                break;
+            }
+        }
+
+        boolean ownerIsSigned = securityService.isSigned(req);
+        if (ownerIsSigned && matchPaths) {
+            res.sendRedirect(req.getContextPath() + "/profile");
+        } else if (!ownerIsSigned && !matchPaths) {
+            res.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
